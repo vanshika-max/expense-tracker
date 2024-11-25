a819cafc54ad1dedfda266314f68500da6139d13
vanshika-max <vanshikaparjapati1@gmail.com>
2024-11-25 11:44:05 +0530

    deprecated options removed

--- server/server.js
+++ server/server.js
@@ -14,6 +14,9 @@ app.use('/api/expenses', expenseRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch(err => console.error(err));
+
+
+    // , { useNewUrlParser: true, useUnifiedTopology: true }
